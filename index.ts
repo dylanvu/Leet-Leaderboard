cf750e760d36a42667910c9d6d021c47997e78f0
Dylan Vu <dylanvu9@gmail.com>
2024-09-05 20:11:35 -0700

    reinstate the build step

--- index.ts
+++ index.ts
@@ -2,47 +2,59 @@ import { Client, Events, GatewayIntentBits } from "discord.js";
 import { configDotenv } from "dotenv";
 import commandDirectory from "./commands";
 import express from "express";
+import { Logging } from "@google-cloud/logging";
 
-configDotenv();
-const TOKEN = process.env.TOKEN;
+const logger = new Logging();
 
-const APP = express();
-const PORT: number = 3000;
+try {
+  configDotenv();
+  const TOKEN = process.env.TOKEN;
 
-APP.get("/", (req: any, res: any) => res.send("App Engine Bot!"));
-APP.listen(PORT, () =>
-  console.log(`Discord bot app listening at http://localhost:${PORT}`)
-);
+  const APP = express();
+  const PORT: number = 3000;
 
-const client = new Client({
-  intents: GatewayIntentBits.Guilds,
-});
+  APP.get("/", (req: any, res: any) => res.send("App Engine Bot!"));
+  APP.listen(PORT, () =>
+    console.log(`Discord bot app listening at http://localhost:${PORT}`)
+  );
 
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  const client = new Client({
+    intents: GatewayIntentBits.Guilds,
+  });
 
-  // Register commands
-  // commandDirectory is an object with values as ICommand objects
-  // turn the object into an array of ICommand objects
-  const commands = Object.values(commandDirectory);
-  // map to data to set commands
-  const commandsData = commands.map((command) => command.data);
-  client.application?.commands.set(commandsData);
-});
+  client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
+    // Register commands
+    // commandDirectory is an object with values as ICommand objects
+    // turn the object into an array of ICommand objects
+    const commands = Object.values(commandDirectory);
+    // map to data to set commands
+    const commandsData = commands.map((command) => command.data);
+    client.application?.commands.set(commandsData);
+  });
 
-  // Get the command object from the commandDirectory
-  const command = commandDirectory[interaction.commandName];
-  if (!command) {
-    interaction.reply("Something went wrong!");
-  }
+  client.on(Events.InteractionCreate, (interaction) => {
+    try {
+      if (!interaction.isChatInputCommand()) return;
+      console.log(interaction);
 
-  // Execute the command
-  command.execute(interaction);
-});
+      // Get the command object from the commandDirectory
+      const command = commandDirectory[interaction.commandName];
+      if (!command) {
+        interaction.reply("Something went wrong!");
+      }
 
-// Log in to Discord with your client's token
-client.login(TOKEN);
+      // Execute the command
+      command.execute(interaction);
+    } catch (error) {
+      logger.log("interactionCreate error: " + error);
+    }
+  });
+
+  // Log in to Discord with your client's token
+  client.login(TOKEN);
+} catch (error) {
+  console.log(error);
+  logger.log("General error: " + error);
+}
